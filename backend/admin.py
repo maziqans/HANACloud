c6617d53ee78d5f287ac884b18331552c7771609
maziq <ammarannas02@gmail.com>
2026-05-11 14:31:24 +0800

    fixed backend fully

---
+++ backend/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import CloudFile
+
+@admin.register(CloudFile)
+class CloudFileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'category', 'file_size', 'uploaded_at')
+    list_filter = ('category', 'user')
--- backend/views.py
+++ backend/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from django.db.models import Sum
-from .models import CloudFile
+from core.models import CloudFile
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
